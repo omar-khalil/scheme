495258f573c45f4eb1377b5ca97834a7ad3f7083
Omar Khalil <omar-khalil@live.co.uk>
2024-01-29 20:31:59 +0300

    wip template for multiple different response codes

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,14 +1,33 @@
-import {SomeZodObject, z} from "zod";
+import {SomeZodObject, ZodAny, ZodLiteral, ZodNumber, ZodObject, ZodRecord, number, z} from "zod";
 import {objects} from "./objects";
 
 type api_method = "put" | "post" | "patch" | "get" | "delete";
+
+const open_pr = z.object({type: z.literal('open'), items: z.string()});
+const closed_pr = z.object({type: z.literal('closed'), closed_date: z.number()});
+const pr_schema = z.union([open_pr, closed_pr]);
+
+type ZodResponse = Record<number, SomeZodObject>;
+
+const exampleResponseSchema = {
+  200: z.object({
+    a: z.number(),
+  }),
+  404: z.object({
+    message: z.string(),
+  })
+} satisfies ZodResponse;
+
+type example_inferred_response = {
+  [T in keyof typeof exampleResponseSchema]: z.infer<(typeof exampleResponseSchema)[T]>
+};
 
 type ZodEndpoint = {
   url: string;
   type: api_method;
   schema: {
     params: SomeZodObject;
-    response: SomeZodObject;
+    response: ZodResponse;
   };
 };
 
@@ -20,7 +39,12 @@ export const endpoint_schema = {
       params: z.object({
         user_id: z.string(),
       }),
-      response: objects.user,
+      response: {
+        200: objects.user,
+        404: z.object({
+          message: z.string(),
+        })
+      }
     }
   },
   post_user: {
@@ -28,9 +52,11 @@ export const endpoint_schema = {
     type: "post",
     schema: {
       params: objects.user,
-      response: z.object({
-        user_id: z.string(), //backend could respond with an id for the created user
-      }),
+      response: {
+        200: z.object({
+          user_id: z.string(), //backend could respond with an id for the created user
+        }),
+      }
     }
   },
   delete_user: {
@@ -40,12 +66,25 @@ export const endpoint_schema = {
       params: z.strictObject({
         user_id: z.string(),
       }),
-      response: z.strictObject({
-        message: z.string(),
-      }),
+      response: {
+        200: z.object({
+          message: z.string(),
+        }),
+      }
     }
   },
 } satisfies Record<string, ZodEndpoint>;
 
 export type params<T extends keyof typeof endpoint_schema> = z.infer<(typeof endpoint_schema)[T]['schema']['params']>;
-export type response<T extends keyof typeof endpoint_schema> = z.infer<(typeof endpoint_schema)[T]['schema']['response']>;
+export type response<T extends keyof typeof endpoint_schema, U extends keyof (typeof endpoint_schema)[T]['schema']['response']> = z.infer<(typeof endpoint_schema)[T]['schema']['response'][U]>;
+
+const asdasd: response<'get_user', 404> = {
+  message: 'abc',
+};
+
+type U<T extends keyof typeof endpoint_schema> = keyof (typeof endpoint_schema)[T]['schema']['response'];
+const bla: U<'get_user'> = 404;
+
+const handle_post_response: response<'get_user', 200> = {
+  name: 'a'
+}
